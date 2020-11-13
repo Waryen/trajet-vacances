d7f09ea699b2565f016c51ffe97980128707100a
Jonathan Gomand <jonathangomand@gmail.com>
2020-11-13 18:55:29 +0100

    Début du projet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // Constructeur
+  constructor() {
+    super()
+    this.state = {
+      lieux: []
+    }
+  }
+
+  render() {
+
+    // Rendu affiché dans le navigateur
+    return (
+      <div className='App'>
+        <h1>Trajet du van</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
